b528c564b06cff6654c92e4bd856f1d3941888e6
ricocheting <root@keeper>
2017-11-06 16:19:16 -0700

    file to process is now flag

--- parser.go
+++ parser.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/ricocheting/logparse/ngparser"
 )
 
 func main() {
-	f, err := os.Open("junk/short.access.log")
+	logfile := flag.String("log", "", "Log file to process")
+	flag.Parse()
+
+	f, err := os.Open(*logfile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,15 +26,15 @@ func main() {
 	/*p.Parse(f, func(r *ngparser.Record) {
 		// if you wanna do some processing to the record
 	})*/
-
+	fmt.Println(time.Now().Format(time.RFC850))
 	fmt.Printf("Hits: %+v\n", p.Count())
 	//fmt.Printf("Top Files: %+v\n", p.Stats(ngparser.Pages, 1000))
 	fmt.Printf("Unique Files: %+v\n", p.StatsCount(ngparser.Pages))
-	fmt.Printf("Unique Extentions: %+v\n", p.StatsCount(ngparser.Extensions))
-	fmt.Printf("All Extentions: %+v\n", p.Stats(ngparser.Extensions, 0))
+	//fmt.Printf("Unique Extentions: %+v\n", p.StatsCount(ngparser.Extensions))
+	//fmt.Printf("All Extentions: %+v\n", p.Stats(ngparser.Extensions, 0))
 
-	fmt.Printf("Unique StatusCodes: %+v\n", p.StatsCount(ngparser.StatusCodes))
-	fmt.Printf("All StatusCodes: %+v\n", p.Stats(ngparser.StatusCodes, 0))
+	//fmt.Printf("Unique StatusCodes: %+v\n", p.StatsCount(ngparser.StatusCodes))
+	//fmt.Printf("All StatusCodes: %+v\n", p.Stats(ngparser.StatusCodes, 0))
 
 	//fmt.Println(p.IPsCount())
 }
